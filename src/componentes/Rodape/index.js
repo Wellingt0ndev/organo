f0ec29efd4ce3f733dac7c5b45fa9ebe52b997c8
Wellington Ferreira da Silva <wellington_006@hotmail.com>
2023-10-03 11:02:26 -0300

    update

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -2,33 +2,7 @@ import './Rodape.css'
 
 const Rodape = () => {
     return (<footer className="footer">
-        <section>
-            <ul>
-                <li>
-                    <a href="facebook.com" target="_blank">
-                        <img src="/imagens/fb.png" alt="" />
-                    </a>
-                </li>
-                <li>
-                    <a href="twitter.com" target="_blank">
-                        <img src="/imagens/tw.png" alt="" />
-                    </a>
-                </li>
-                <li>
-                    <a href="instagram.com" target="_blank">
-                        <img src="/imagens/ig.png" alt="" />
-                    </a>
-                </li>
-            </ul>
-        </section>
-        <section>
-            <img src="/imagens/logo.png" alt="" />
-        </section>
-        <section>
-            <p>
-                Desenvolvido por Wellington.
-            </p>
-        </section>
+        <img src="/imagens/valorant-final.jpg" alt='Imagem do footer' />        
     </footer>)
 }
 
