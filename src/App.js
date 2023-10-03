242f94df3c19c2fdbf0461e32e42ede8b27e8ab9
Wellington Ferreira da Silva <wellington_006@hotmail.com>
2023-10-03 10:37:29 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
+import Rodape from './componentes/Rodape';
 
 function App() {
+
+  const times = [
+    {
+      nome:'Iniciador' ,
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome:'Duelista', 
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome:'Controlador', 
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome:'Sentinela',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE5F5'
+    }
+  ]
+
+  const [jogadores, setJogadores] = useState([])
+
+  const aoNovoJogadorAdionado = (jogador) =>{
+    console.log(jogador)
+    setJogadores([...jogadores, jogador]);
+  }
   return (
     <div className="App">
       <Banner/>  
-      <Formulario /> 
+      <Formulario times={times.map(time => time.nome)} aoJogadorCadastrado={jogador => aoNovoJogadorAdionado(jogador)} /> 
+      {times.map(time => <Time 
+      key={time.nome} 
+      nome={time.nome} 
+      corPrimaria = {time.corPrimaria} 
+      corSecundaria = {time.corSecundaria}
+      jogadores = {jogadores.filter(jogador => jogador.time === time.nome)}
+      />)}
+      <Rodape />      
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,30 +1,63 @@
+import { useState } from "react"
 import Botao from "../Botao"
 import CampoTexto from "../CampoTexto"
 import ListaSuspensa from "../ListaSuspensa"
 import './Formulario.css'
 
-const Formulario = () => {
-    const times = [
-        '',
-        'Iniciador',
-        'Duelista',
-        'Controlador',
-        'Sentinela'
-    ]
+const Formulario = (props) => {
+    
+    const[nome, setNome] = useState('')
+    const[cargo, setCargo] = useState('')
+    const[imagem, setImagem] = useState('')
+    const[time, setTime] = useState('')
+    
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('foi feito o submite')
+        props.aoJogadorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+    })
+    setNome('')
+    setCargo('')
+    setImagem('')
+    setTime('')
     }
 
     return(
     <section className="formulario">
         <form onSubmit={aoSalvar}>
             <h2>Preencha os dados para o card dos jogadores da DCG</h2>
-            <CampoTexto obrigatorio = {true} label="Nome" placeholder = "Digite seu nome" />    
-            <CampoTexto obrigatorio = {true} label="Cargo" placeholder = "Digite seu cargo" />    
-            <CampoTexto obrigatorio = {true} label="Imagem" placeholder = "Digite o endereço da imagem" />   
-            <ListaSuspensa obrigatorio = {true} label="Time" itens = {times} />
+            <CampoTexto
+                obrigatorio = {true}
+                label="Nome" 
+                placeholder = "Digite seu nome" 
+                valor = {nome}
+                aoAlterado = {valor => setNome(valor)}
+            />    
+            <CampoTexto
+                obrigatorio = {true} 
+                label="Cargo" 
+                placeholder = "Digite seu cargo" 
+                 valor = {cargo}
+                aoAlterado = {valor => setCargo(valor)}
+            />    
+            <CampoTexto 
+                obrigatorio = {true} 
+                label="Imagem" 
+                placeholder = "Digite o endereço da imagem" 
+                valor = {imagem}
+                aoAlterado = {valor => setImagem(valor)}
+            />   
+            <ListaSuspensa
+             obrigatorio = {true} 
+             label="Time" 
+             itens = {props.times}
+             valor = {time}
+             aoAlterado = {valor => setTime(valor)}
+             />
             <Botao> 
                 Criar Card
             </Botao>            
